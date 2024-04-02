0651e6fa3a306a94f254ae07d2b447d419ebd485
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2024-04-02 16:12:14 -0500

    gitignore: added private templates

---
+++ .bin/Add-Private-Template-Symlinks.ps1
@@ -0,0 +1,48 @@
+# Check if script is running as administrator, if not, relaunch as administrator
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Host "Script is not running as administrator. Relaunching with administrator privileges..."
+    Start-Process powershell.exe -Verb RunAs -ArgumentList ("-File", $MyInvocation.MyCommand.Path)
+    exit
+}
+
+echo "Script root:"
+echo $PSScriptRoot
+
+# Define source and destination directories
+$sourceDirectory = "$PSScriptRoot\..\private\.foam\templates"
+$destinationDirectory = "$PSScriptRoot\..\.foam\templates"
+
+echo $sourceDirectory
+
+echo $destinationDirectory
+
+# Check if the source directory exists
+if (-not (Test-Path -Path $sourceDirectory -PathType Container)) {
+    Write-Host "Source directory not found."
+    exit
+}
+
+# Check if the destination directory exists, if not, create it
+if (-not (Test-Path -Path $destinationDirectory -PathType Container)) {
+    New-Item -Path $destinationDirectory -ItemType Directory -Force | Out-Null
+}
+
+# Get all files in the source directory
+$files = Get-ChildItem -Path $sourceDirectory -File
+
+# Loop through each file and create symlink in destination directory
+foreach ($file in $files) {
+    $linkPath = Join-Path -Path $destinationDirectory -ChildPath $file.Name
+    $targetPath = $file.FullName
+
+    # Check if symlink already exists
+    if (-not (Test-Path -Path $linkPath -PathType Leaf)) {
+        # Create symlink
+        New-Item -ItemType SymbolicLink -Path $linkPath -Value $targetPath
+        Write-Host "Symlink created for $($file.Name)"
+    } else {
+        Write-Host "Symlink already exists for $($file.Name)"
+    }
+}
+
+pause
